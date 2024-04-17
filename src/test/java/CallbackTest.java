bb36ea75d9f90cc22557e333c89469d4c99256db
sergei <osp11011983@mail.ru>
2024-04-17 19:30:18 +0200

    add WebDriverManager

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,5 +1,3 @@
-package ru.netology.web;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -40,25 +38,34 @@ class CallbackTest {
     }
 
     @Test
-    void shouldTestV1() {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    void testWebSiteHappyPath() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input ")).sendKeys("Николай Булкин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[role=button]")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
-
     @Test
-    void shouldTestV2() {
-        WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Василий");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.cssSelector("[data-test-id=submit]")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    void testWebSiteBadPath_1() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input ")).sendKeys("Kolya Bylkin");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[role=button]")).click();
+        String text = driver.findElement(By.cssSelector("[class=input__sub]")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+    @Test
+    void testWebSiteBadPath_2() {
+        driver.get("http://localhost:9999/");
+        driver.findElement(By.cssSelector("[data-test-id=name] input ")).sendKeys("Николай Булкин");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input ")).sendKeys("+799988877666");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("[role=button]")).click();
+        WebElement form = driver.findElement(By.cssSelector("[data-test-id=phone]"));
+        String text = form.findElement(By.cssSelector("[class=input__sub]")).getText();
+        assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
 }
-
